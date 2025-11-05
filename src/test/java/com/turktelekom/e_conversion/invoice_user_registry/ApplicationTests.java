6018c7a4d9392e3be80513566315114280f4d2ed
serkan.avci <serkan.avci@ithinka.com>
2025-11-05 16:03:04 +0300

    Project created

---
+++ src/test/java/com/turktelekom/e_conversion/invoice_user_registry/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turktelekom.e_conversion.invoice_user_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
